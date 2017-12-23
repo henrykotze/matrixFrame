8238248c0c0f2e74f48363ec9ca80347a7841ab4
Henry Kotze <henry@localhost.localdomain>
2017-12-23 22:12:50 +0200

    Initial commit of 'playground work'

---
+++ README.md
@@ -0,0 +1,17 @@
+# matrixFrame
+
+matrixFrame tries to create a "The Matrix" art of a input frame. As seen here: 
+
+![alt text](http://www.citizenop.com/wp-content/uploads/2016/03/the-matrix.jpg)
+
+## Current Developing
+----
+
+* Currently creating a playground for implementing the idea
+* Development is done using openCV 3.2.0
+
+
+## Future Plans
+----
+
+*	Use ML to generate matrix embedded images/videos
---
+++ opencv_test.cpp
@@ -0,0 +1,216 @@
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/core/utility.hpp"
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+// Globals 
+cv::Mat image;
+cv::Mat image_gray;
+cv::Mat image_copy;
+
+int thresh = 100;
+int max_thresh = 255; 
+cv::RNG rng(12345);
+
+//globals for trackbars
+int fontsize = 2 ; 
+int max_fontsize = 255 ; 
+
+
+
+void threshold_callback(int, void*);
+void textsize_callback(int, void*);
+void fillText(cv::Mat frame, double fontsize);
+
+vector<vector<cv::Point>> getContours(cv::Mat frame);
+
+
+
+int main( int argc, char** argv )
+{
+    
+    string imageName("/home/henry/Documents/opencv/matrixFrame/opencv_nic2.jpg"); // by default
+
+
+    int exit = 0;
+    if( argc > 1)
+    {
+        imageName = argv[1];
+    }
+
+    // Defining image objects
+
+    //Loading Image to be Processed
+    image = cv::imread(imageName.c_str(), cv::IMREAD_COLOR); // Read the file
+    image_copy = image;
+    vector<int> image_size(image.cols, image.rows);
+
+    cout << "image cols: " << image_size.at(1) << " image rows: " << image_size.at(2) << endl;
+
+
+    //Convert image to gray scale
+    cv::cvtColor(image,image_gray,CV_BGR2GRAY);
+
+
+
+
+
+
+    if( image.empty() )                      // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+    // Creating Windows
+ //   cv::namedWindow( "original image", cv::WINDOW_AUTOSIZE );
+
+    string text("meep");
+    //insert some text
+//    cv::putText(image, text, cv::Point(0,0), cv::FONT_HERSHEY_DUPLEX, fontsize, cv::Scalar(255, 0,3), 1, cv::LINE_8);
+
+    fillText(image, 7);
+
+
+    // Show images
+//    cv::imshow( "original image", image );
+//    cv::createTrackbar( " Canny threshold:", "original image", &thresh, max_thresh, threshold_callback );
+//   cv::createTrackbar( " Font Size:", "original image", &fontsize, max_fontsize, textsize_callback );
+//   threshold_callback( 0, 0 );  
+
+    cout << "To exit press Escape \n";
+    while(exit != 27){
+        exit = cv::waitKey(0); // Wait for Escape to exit
+    }
+    return 0;
+}
+
+void threshold_callback(int, void*){
+
+    cv::Mat canny_output;
+    vector<vector<cv::Point>> contours;
+    vector<cv::Vec4i> hierarchy;
+    
+    //Canny the image
+    cv::Canny(image_gray, canny_output, thresh, thresh*2,3);
+
+    //Detect contours and store in memory
+    cv::findContours(canny_output, contours, hierarchy, CV_RETR_TREE,CV_CHAIN_APPROX_SIMPLE, cv::Point(0,0));
+
+
+    cv::Mat drawing = cv::Mat::zeros( canny_output.size(), CV_8UC3);
+
+    //Draw Contours
+    for(int i = 0; i < contours.size(); i++){
+       cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+       cv::drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, cv::Point() );
+       cv::drawContours( image_copy, contours, i, color, 1, 8, hierarchy, 0, cv::Point() );
+
+       vector<cv::Point> position = contours.at(i);
+        //Gives position (x,y) for each pixel in a countour
+       for(size_t meep = 0; meep < position.size(); meep++){
+           cv::Point point = position.at(meep);
+           //cout << point.x << " " << point.y << endl;
+       }
+
+
+       //int proc = cv::waitKey(0);
+       //cout << proc << endl;
+    }
+
+
+
+
+
+    cv::namedWindow("Contours on Original", CV_WINDOW_AUTOSIZE);
+    cv::namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+    cv::imshow( "Contours", drawing );
+    cv::imshow("Contours on Original", image_copy);
+
+
+}
+
+void textsize_callback(int, void*){
+
+    string text("meep");
+    double change_font = double(fontsize)/255;
+    int baseline = 0;
+    //cv::Mat textImage;
+    //textImage = image;
+
+    cv::Mat textImage = cv::Mat::zeros( image.size(), CV_8UC3);
+
+    //insert some text
+    cv::putText(textImage, text, cv::Point(20,20), cv::FONT_HERSHEY_DUPLEX, change_font, cv::Scalar(255, 0,3), 1, cv::LINE_8);
+    //cv::putText(image, "abcdefghijklmnopqrstuvxyz0123456789", cv::Point(1,0), cv::FONT_HERSHEY_DUPLEX, fontsize, cv::Scalar(255, 0,3), 1, cv::LINE_8);
+
+    cv::Size fontsize = cv::getTextSize(text,cv::FONT_HERSHEY_DUPLEX,change_font,1,&baseline);
+
+    cout << "height: " << fontsize.height << " widht: " << fontsize.width << endl;
+    
+
+    cv::namedWindow("Edit",CV_WINDOW_AUTOSIZE);
+    cv::imshow("Edit", textImage);
+
+}
+
+
+void fillText(cv::Mat frame, double font){
+    double rows = double(frame.rows);
+    double cols = double(frame.cols);
+    char test = 100;
+    string text(&test);
+
+    vector<cv::Point> textPos;
+
+
+
+
+    int cols_steps = cols/font;
+    int rows_steps = rows/font;
+    cout << "Number of cols_steps: " << cols_steps << endl;
+    cout << "Number of rows_steps: " << rows_steps << endl;
+    
+    int rows_iter = 0;
+    int cols_iter = font;
+
+    for(int j = 0; j < cols_steps; j++){
+        for(int i = 0; i < rows_steps ; i++){
+            text = char(int(rng.uniform(0, 127)));
+            cout << "cols: " << cols_iter << " rows: " << rows_iter << endl;
+            cv::putText(frame, text, cv::Point(cols_iter,rows_iter), cv::FONT_HERSHEY_DUPLEX, 0.25, cv::Scalar(80, 100 , 30), 1, cv::LINE_8);
+            rows_iter = rows_iter + font;
+        }
+        cols_iter = cols_iter + font; 
+        rows_iter = 0 ;
+
+    }
+
+
+    cv::namedWindow("filled rows", CV_WINDOW_AUTOSIZE);
+    cv::imshow("filled rows", frame);
+
+}
+vector<vector<cv::Point>> getContours(cv::Mat frame){
+
+    cv::Mat canny_output;
+    vector<vector<cv::Point>> contours;
+    vector<cv::Vec4i> hierarchy;
+    
+    //Canny the image
+    cv::Canny(image_gray, canny_output, thresh, thresh*2,3);
+
+    //Detect contours and store in memory
+    cv::findContours(canny_output, contours, hierarchy, CV_RETR_TREE,CV_CHAIN_APPROX_SIMPLE, cv::Point(0,0));
+
+    return contours;    
+}
