17eeea0260428569f7dd657592869a9f5d95fe04
Henry Kotze <henryykotze@gmail.com>
2017-12-28 22:53:36 +0200

    stderr stream output to stdout stream

--- compile.sh
+++ compile.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 
-echo "Creating build dir"
+echo "Creating build dir" | tee -a ./build/compile.log
 mkdir -p ./build
 
-echo "Compiling element.cpp"
-g++ -c ./dep/element.cpp `pkg-config --libs opencv` -std=c++11 -o ./build/element.o  | tee -a ./build/compile.log
+echo "Compiling element.cpp" | tee -a ./build/compile.log
+g++ -c ./dep/element.cpp `pkg-config --libs opencv` -std=c++11 -o ./build/element.o 2>&1  | tee -a ./build/compile.log
 
-echo "Compiling functions.cpp"  | tee -a compile.log
-g++ -c ./dep/functions.cpp `pkg-config --libs opencv` -std=c++11 -o ./build/functions.o | tee -a ./build/compile.log
+echo "Compiling functions.cpp"  | tee -a ./build/compile.log
+g++ -c ./dep/functions.cpp `pkg-config --libs opencv` -std=c++11 -o ./build/functions.o 2>&1 | tee -a ./build/compile.log
 
-echo "Compiling main.cpp" | tee -a compile.log
-g++ -c main.cpp `pkg-config --libs opencv` -std=c++11 -o ./build/main.o| tee -a ./build/compile.log
+echo "Compiling main.cpp" | tee -a ./build/compile.log
+g++ -c main.cpp `pkg-config --libs opencv` -std=c++11 -o ./build/main.o 2>&1 | tee -a ./build/compile.log
 
-echo "Linking object files" | tee -a compile.log
-g++ -std=c++11 `pkg-config --libs opencv` ./build/*.o -o matrixFrame | tee -a ./build/compile.log
+echo "Linking object files" | tee -a ./build/compile.log
+g++ -std=c++11 `pkg-config --libs opencv` ./build/*.o -o matrixFrame 2>&1 | tee -a ./build/compile.log
 
 echo "Done"
