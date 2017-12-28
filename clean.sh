5d67945dd836e0f8cb5620fefb606ae5a3e5d2e9
Henry Kotze <henryykotze@gmail.com>
2017-12-28 16:28:17 +0200

    builds compiling process in new build dir

--- clean.sh
+++ clean.sh
@@ -3,14 +3,14 @@
 echo "Cleaning building process"
 
 echo "Removing Object Files:"
-ls ./*.o
-rm ./*.o
+ls ./build/*.o
+rm ./build/*.o
 
 echo "Removing execurtable binary"
 rm ./matrixFrame
 
 echo "Removing compile.log"
-rm ./compile.log
+rm ./build/compile.log
 
 echo "Done"
 
